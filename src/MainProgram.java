cc4fff8f9ff254f370d9ea6582b25c3c86d4f58c
mrdt.07 <belomaridiat@gmail.com>
2023-05-23 05:54:45 +0200

    methode 6 & 7

--- src/MainProgram.java
+++ src/MainProgram.java
@@ -163,6 +163,14 @@ class MainProgram {
          * ob es sich bei dem Array um ein Palindrom handelt.
          * Die Methode soll einen Wert des Typs boolean zurückgeben.
          */
+        private static boolean checkArray(int[] array){
+            boolean sort = false;
+            for (int i = 0; i < array.length; i++){
+                if (array.length < array[i]) {
+                    sort = true;
+                }
+            }return sort;
+        }
 
         /** 9. Erhöhen der Inhalte eines Feldes
          * Schreiben Sie eine Methode increaseArray, die als Parameter ein Array des Typs int und einen int-Wert erhält.
@@ -170,5 +178,12 @@ class MainProgram {
          * Beispiel: Werden ein Array mit den Elementen 80,7,1,56,11,72,43,37 als erstes und der Wert 17 als zweites Argument übergeben,
          * so soll ein neues(!) Array mit den Werten 97,24,18,73,28,89,60,54 zurückgegeben werden.
          */
+        private static int increaseArray(int[] array,int number){
+           number = 15;
+           int newArray = 0;
+           for(int i = 0; i < array.length; i++){
+               newArray = array.length + number;
+           }return newArray;
+        }
 
 }
